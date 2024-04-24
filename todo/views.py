a4351a452409a794de2c5cdf55c99811b0d25582
Andrii Zubchenko <152086790+dissom@users.noreply.github.com>
2024-04-24 22:09:53 +0300

    add models

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def index(request) -> HttpResponse:
+    """View function for the home page of the site."""
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_visits": num_visits,
+    }
+
+    return render(request, template_name="todo/index.html", context=context)
